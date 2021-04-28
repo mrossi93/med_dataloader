af6490f5e838c99e659086ee447a323b478ae245
Matteo Rossi <rossimatteo1993@gmail.com>
2021-04-28 17:20:32 +0200

    Fixed some TODO. Improved flexibility in input dimension and data augmentation

--- med_dataloader/med_dataloader.py
+++ med_dataloader/med_dataloader.py
@@ -9,17 +9,17 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 
 class DataLoader:
-    """[summary]
-    """
+    """[summary]"""
 
-    def __init__(self,
-                 imgA_label,
-                 imgB_label,
-                 data_dir='./Data',
-                 imgs_subdir='Images',
-                 cache_mode="prod",
-                 extract_only=None,
-                 ):
+    def __init__(
+        self,
+        imgA_label,
+        imgB_label,
+        data_dir="./Data",
+        imgs_subdir="Images",
+        cache_mode="prod",
+        extract_only=None,
+    ):
         """[summary]
 
         Args:
@@ -32,7 +32,7 @@ class DataLoader:
             data_dir (str, optional): Path to directory that contains the
                 Dataset. This folder **must** contain a subfolder named like
                 :py:attr:`imgs_subdir`. Defaults to './Data'.
-            imgs_subdir (str, optional): Name (**not** the entire path) of the 
+            imgs_subdir (str, optional): Name (**not** the entire path) of the
                 folder that actually contains :py:attr:`imgA_label` and
                 :py:attr:`imgB_label` subfolders. It's a subfolder of Defaults
                 to 'Images'.
@@ -87,35 +87,43 @@ class DataLoader:
             self.imgB_paths = self.imgB_paths[:extract_only]
 
     def get_dataset(self,
+                    input_size,
                     batch_size=32,
+                    norm_bounds=None,
                     augmentation=False,
-                    crop_size=128):
+                    random_crop_size=None,
+                    random_rotate=False,
+                    random_flip=False):
 
-        ds = tf.data.Dataset.zip((self.get_imgs(img_paths=self.imgA_paths),
-                                  self.get_imgs(img_paths=self.imgB_paths)
+        ds = tf.data.Dataset.zip((self.get_imgs(img_paths=self.imgA_paths,
+                                                norm_bounds=norm_bounds),
+                                  self.get_imgs(img_paths=self.imgB_paths,
+                                                norm_bounds=norm_bounds)
                                   ))
 
-        ds = ds.map(self.check_dims, num_parallel_calls=AUTOTUNE)
+        ds = ds.map(lambda imgA, imgB: self.check_dims(imgA,
+                                                       imgB,
+                                                       input_size),
+                    num_parallel_calls=AUTOTUNE)
 
         if augmentation:
             # TODO selectively choose which type of data augmentation to apply
-            # ds = ds.map(lambda imgA, imgB: self.random_crop(imgA,
-            #                                                imgB,
-            #                                                crop_size),
-            #            num_parallel_calls=AUTOTUNE)
-            ds = ds.map(lambda imgA, imgB: self.resize(imgA,
-                                                       imgB,
-                                                       crop_size),
-                        num_parallel_calls=AUTOTUNE)
-            ds = ds.map(self.random_rotate, num_parallel_calls=AUTOTUNE)
-            ds = ds.map(self.random_flip, num_parallel_calls=AUTOTUNE)
+            if random_crop_size:
+                ds = ds.map(lambda imgA, imgB: self.random_crop(imgA,
+                                                                imgB,
+                                                                random_crop_size),
+                            num_parallel_calls=AUTOTUNE)
+            if random_rotate:
+                ds = ds.map(self.random_rotate, num_parallel_calls=AUTOTUNE)
+            if random_flip:
+                ds = ds.map(self.random_flip, num_parallel_calls=AUTOTUNE)
 
         ds = ds.batch(batch_size)
         ds = ds.prefetch(buffer_size=AUTOTUNE)
 
         return ds
 
-    def get_imgs(self, img_paths):
+    def get_imgs(self, img_paths, norm_bounds=None):
         """Open image files for one class and store it inside cache.
 
         This function performs all the (usually) slow reading operations that
@@ -139,14 +147,20 @@ class DataLoader:
         # Get parent folder name from first element in img_paths
         img_label = os.path.basename(os.path.split(img_paths[0])[0])
 
-        cache_file = os.path.join(self.cache_dir, f'{img_label}.cache')
-        index_file = f'{cache_file}.index'
+        cache_file = os.path.join(self.cache_dir, f"{img_label}.cache")
+        index_file = f"{cache_file}.index"
 
         ds = tf.data.Dataset.from_tensor_slices(img_paths)
         ds = ds.map(lambda path: tf.py_function(self.open_img,
                                                 [path],
                                                 [tf.float32]),
                     num_parallel_calls=AUTOTUNE)
+
+        if norm_bounds is not None:
+            ds = ds.map(lambda imgA: self.norm_with_bounds(imgA,
+                                                           norm_bounds),
+                        num_parallel_calls=AUTOTUNE
+                        )
         ds = ds.cache(cache_file)
 
         if not os.path.exists(index_file):
@@ -163,7 +177,7 @@ class DataLoader:
                 when images are named with a progressive number inside a folder
                 (e.g.: 001.xxx, 002.xxx, ..., 999.xxx)
         """
-        print("Fetching images paths...")
+        # print("Fetching images paths...")
         subset_dir = os.path.join(self.data_dir, self.imgs_subdir)
 
         subset_dir_imgA = os.path.join(subset_dir, self.imgA_label)
@@ -177,7 +191,7 @@ class DataLoader:
         paths_imgB = [os.path.join(subset_dir_imgB, img)
                       for img in filenames_imgB]
 
-        print("Images paths collected.")
+        # print("Images paths collected.")
 
         # Sort paths alphabetically
         paths_imgA.sort()
@@ -201,48 +215,48 @@ class DataLoader:
 
         # Scale image in 0-1 with a predefined range
         # TODO: remove hardcoding in this function
-        image = self.normalise_with_boundaries(image, lb=-1024, ub=3200)
+        # image = self.normalise_with_boundaries(image, lb=-1024, ub=3200)
         tensor = tf.convert_to_tensor(image)
         return tf.expand_dims(tensor, axis=-1)
 
     def _populate_cache(self, ds, cache_file, num_tot):
-        print(f'Caching decoded images in {cache_file}...')
+        print(f"Caching decoded images in {cache_file}...")
         i = 0
         for _ in ds:
             i += 1
-            sys.stdout.write('\r')
+            sys.stdout.write("\r")
             sys.stdout.write(f"{i}/{num_tot}")
             sys.stdout.flush()
-        print(f'\nCached decoded images in {cache_file}.')
+        print(f"\nCached decoded images in {cache_file}.")
 
     # -----------------------------------------------------------
     #  Transformations
     # -----------------------------------------------------------
     @staticmethod
-    def check_dims(imgA, imgB):
-        # TODO: remove hardcoding
+    def check_dims(imgA, imgB, size):
         imgA = tf.expand_dims(tf.squeeze(imgA), axis=-1)
-        imgA = tf.image.resize_with_pad(imgA, 256, 256)
+        imgA = tf.image.resize_with_pad(imgA, size, size)
 
         imgB = tf.expand_dims(tf.squeeze(imgB), axis=-1)
-        imgB = tf.image.resize_with_pad(imgB, 256, 256)
+        imgB = tf.image.resize_with_pad(imgB, size, size)
 
         return imgA, imgB
 
     @staticmethod
-    def normalise_with_boundaries(image, lb, ub):
+    def norm_with_bounds(image, bounds):
         """Image normalisation. Normalises image in the range defined by lb and
         ub to fit [0, 1] range."""
         epsilon = 1e-8
+        lb = bounds[0]
+        ub = bounds[1]
 
-        image[image < lb] = lb
-        image[image > ub] = ub
+        tf.where(image < lb, lb, image)
+        tf.where(image > ub, ub, image)
 
-        # image = image.astype(np.float32)
-        ret = image - lb
-        ret /= ((ub - lb) + epsilon)
+        image = image - lb
+        image /= (ub - lb) + epsilon
 
-        return ret
+        return image
 
     @staticmethod
     def resize(imgA, imgB, size=256):
@@ -255,7 +269,8 @@ class DataLoader:
     def random_crop(imgA, imgB, crop_size=256):
         stacked_img = tf.stack([imgA, imgB], axis=0)
         cropped_img = tf.image.random_crop(
-            stacked_img, size=[2, crop_size, crop_size, 1])
+            stacked_img, size=[2, crop_size, crop_size, 1]
+        )
         cropped_img = tf.split(cropped_img, 2)
         imgA = tf.expand_dims(tf.squeeze(cropped_img[0]), axis=-1)
         imgB = tf.expand_dims(tf.squeeze(cropped_img[1]), axis=-1)
@@ -277,9 +292,14 @@ class DataLoader:
 
 
 def generate_dataset(data_loader,
+                     input_size,
                      percentages,
                      batch_size,
-                     input_size
+                     norm_bounds=None,
+                     train_augmentation=True,
+                     random_crop_size=None,
+                     random_rotate=True,
+                     random_flip=True,
                      ):
 
     if len(percentages) != 3:
@@ -299,9 +319,12 @@ def generate_dataset(data_loader,
     valid_ends = valid_begins + int(num_imgsA * percentages[1])
 
     complete_ds = data_loader.get_dataset(batch_size=batch_size,
-                                          augmentation=True,
-                                          crop_size=input_size
-                                          )
+                                          norm_bounds=norm_bounds,
+                                          input_size=input_size,
+                                          augmentation=train_augmentation,
+                                          random_crop_size=random_crop_size,
+                                          random_rotate=random_rotate,
+                                          random_flip=random_flip)
 
     complete_ds = complete_ds.unbatch()
 
@@ -310,6 +333,8 @@ def generate_dataset(data_loader,
     train_ds = train_ds.batch(batch_size)
 
     complete_ds = data_loader.get_dataset(batch_size=batch_size,
+                                          norm_bounds=norm_bounds,
+                                          input_size=input_size,
                                           augmentation=False)
 
     complete_ds = complete_ds.unbatch()
