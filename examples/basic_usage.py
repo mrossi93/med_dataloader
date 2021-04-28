081320af41d7d23523f2aa95239c8f2c59c42e86
Matteo Rossi <rossimatteo1993@gmail.com>
2021-04-28 17:21:11 +0200

    Basic example updated. Now available also as notebook

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -7,7 +7,13 @@ dl = med_dl.DataLoader(imgA_label="CBCT",
                        data_dir="Test_Dataset")
 
 train_ds, valid_ds, test_ds = med_dl.generate_dataset(data_loader=dl,
+                                                      input_size=256,
                                                       percentages=[
                                                           0.8, 0.1, 0.1],
                                                       batch_size=3,
-                                                      input_size=256)
+                                                      norm_bounds=[-1024.0,
+                                                                   3200.0],
+                                                      random_crop_size=128,
+                                                      random_rotate=True,
+                                                      random_flip=True
+                                                      )
